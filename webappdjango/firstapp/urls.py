55ba9193e6346831bdeb077c98f3678168e9149e
Atan Bhardwaj <49301379+atanbhardwaj@users.noreply.github.com>
2021-01-16 23:33:48 +0530

    Delete webappdjango directory

--- webappdjango/firstapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("function", views.function, name='function'),
-    path("<str:name>", views.greet, name="greet"),
-    path("index", views.index, name="index")
-]
